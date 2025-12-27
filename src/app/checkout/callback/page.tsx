6a5135c6b00c9c28807bb33a451d4f5e034a78c5
AgXchange Devstack <devstack@agxchange.co.za>
2025-12-27 06:10:23 +0200

    Update page.tsx
    Paystack payment callback page

--- src/app/checkout/callback/page.tsx
+++ src/app/checkout/callback/page.tsx
@@ -1,1 +1,9 @@
+const reference = searchParams.get('reference');
+const { data } = await mutate(AddPaymentToOrderMutation, {
+    input: { method: 'paystack', metadata: { reference } }
+}, { useAuthToken: true });
 
+if (data?.addPaymentToOrder?.__typename === 'Order') {
+    redirect(`/order-confirmation/${data.addPaymentToOrder.code}`);
+}
+// Display paymentErrorMessage to user
